4f1735c84ffa6ebe2deffcd2a5f9b022b1ddda4c
PriyanshGupta2002 <guptapriyansh23@gmail.com>
2022-08-18 19:28:49 +0530

    fixed

--- pages/api/discover/[topic].ts
+++ pages/api/discover/[topic].ts
@@ -5,7 +5,7 @@ import { topicPostsQuery } from '../../../utils/queries';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method==="GET") {
       const {topic} = req.query;
-      const videosQuery = topicPostsQuery(topic)
+      const videosQuery = topicPostsQuery(topic!)
       const videos = await client.fetch(videosQuery)
         res.status(200).json(videos)
   }
