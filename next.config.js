73cd38964afdc5061486c846a97af78476aa9eae
PriyanshGupta2002 <guptapriyansh23@gmail.com>
2022-08-18 19:56:47 +0530

    fixed

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ module.exports = {
     domains: ['yt3.ggpht.com','lh3.googleusercontent.com'],
   },
 
-
   
   env:{
     NEXT_PUBLIC__GOOGLE_API_TOKEN:'664590646742-bugmc3p7kd691amog74rk9r1vnraa2rp.apps.googleusercontent.com'
