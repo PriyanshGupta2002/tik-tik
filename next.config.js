1b1c53bcbbdfd936777d282c9a69a5ac947cb343
PriyanshGupta2002 <guptapriyansh23@gmail.com>
2022-08-18 19:43:21 +0530

    ...

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ module.exports = {
   images: {
     domains: ['yt3.ggpht.com','lh3.googleusercontent.com'],
   },
+
+
   
   env:{
     NEXT_PUBLIC__GOOGLE_API_TOKEN:'664590646742-bugmc3p7kd691amog74rk9r1vnraa2rp.apps.googleusercontent.com'
